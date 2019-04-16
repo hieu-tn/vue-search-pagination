7e3d456c473ca6605f1ad505f1e63f8347843fc9
Hieu Tran <hieu@construct.sg>
2019-04-16 23:38:40 +0700

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pagination></pagination>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pagination from './components/Pagination.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Pagination
   }
 }
 </script>
