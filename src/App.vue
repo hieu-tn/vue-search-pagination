0dc4f185687dafaa2836180b8e945072eddc720d
Hieu Tran <hieu@construct.sg>
2019-04-18 11:16:29 +0700

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <pagination></pagination>
+    <pagination :list="imdb">
+    </pagination>
   </div>
 </template>
 
@@ -12,17 +12,109 @@ export default {
   name: 'app',
   components: {
     Pagination
+  },
+  data () {
+    return {
+      imdb: [
+        {
+          tconst: "tt10008984",
+          titleType: "short",
+          originalTitle: "Laisse Yves tranquille",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt10061540",
+          titleType: "short",
+          originalTitle: "Very Important Message",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt10075980",
+          titleType: "short",
+          originalTitle: "Les visiteurs",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt10088286",
+          titleType: "short",
+          originalTitle: "Le confessionnal",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt10124684",
+          titleType: "tvEpisode",
+          originalTitle: "Une chance sur deux",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt10163196",
+          titleType: "tvSeries",
+          originalTitle: "The Benza",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9129986",
+          titleType: "short",
+          originalTitle: "Molly",
+          runtimeMinutes: "10"
+        },
+        {
+          tconst: "tt9630260",
+          titleType: "tvEpisode",
+          originalTitle: "No Sleep Sleepover",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9630288",
+          titleType: "tvEpisode",
+          originalTitle: "Camp Unikitty",
+          runtimeMinutes: "11"
+        },
+        {
+          tconst: "tt9659750",
+          titleType: "tvEpisode",
+          originalTitle: "Unfairgrounds Part 1",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9659756",
+          titleType: "tvEpisode",
+          originalTitle: "Unfairgrounds Part 2",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9659762",
+          titleType: "tvEpisode",
+          originalTitle: "Hawkodile Sensei",
+          runtimeMinutes: "11"
+        },
+        {
+          tconst: "tt9659798",
+          titleType: "tvEpisode",
+          originalTitle: "Beep",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9659820",
+          titleType: "tvEpisode",
+          originalTitle: "Safety First",
+          runtimeMinutes: "\\N"
+        },
+        {
+          tconst: "tt9805804",
+          titleType: "tvMovie",
+          originalTitle: "Potpisani 08 - Treger",
+          runtimeMinutes: "57"
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+@import "~bootstrap/dist/css/bootstrap.min.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,12 +1,101 @@
 <template>
   <div>
+    <div class="container">
+      <h1>Integrate search and pagination into VueJS</h1>
+      <div class="form-group">
+        <label for="query">Enter query</label>
+        <input type="text" class="form-control" id="query" placeholder="Enter your query" v-model="s" />
+      </div>
 
+      <div class="table-responsive">
+        <table class="table table-striped table-hover table-bordered">
+          <thead>
+            <tr class="thead-dark">
+              <th scope="col">tconst</th>
+              <th scope="col">Title Type</th>
+              <th scope="col">Original Title</th>
+              <th scope="col">Runtime Minutes</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in displayedList" :key="index">
+              <td>{{ item.tconst }}</td>
+              <td>{{ item.titleType }}</td>
+              <td>{{ item.originalTitle }}</td>
+              <td>{{ item.runtimeMinutes }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item" :class="{ 'disabled': this.currentPage <= 0 }" @click="prevPage"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item" :class="{ 'active': currentPage == page - 1 }" v-for="(page, index) in totalPages" :key="index" @click="moveToPage($event, page)"><a class="page-link" href="#">{{ page }}</a></li>
+          <li class="page-item" :class="{ 'disabled': this.currentPage >= this.totalPages - 1 }" @click="nextPage"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
   name: 'Pagination',
+  props: {
+    list: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+  },
+  data () {
+    return {
+      s: '',
+      itemsPerPage: 5,
+      currentPage: 0
+    }
+  },
+  computed: {
+    filteredList: function() {
+      let resultList = []
+      for (let item of this.list) {
+        for (let property in item) {
+          if (new RegExp(this.s.toLowerCase()).test(item[property])) {
+            resultList.push(item)
+            break
+          }
+        }
+      }
+      return resultList
+    },
+    displayedList: function() {
+      return this.filteredList.slice(this.currentPage * this.itemsPerPage, (this.currentPage + 1) * this.itemsPerPage)
+    },
+    totalPages: function() {
+      this.currentPage = 0
+      return Math.ceil(this.filteredList.length / this.itemsPerPage)
+    }
+  },
+  methods: {
+    moveToPage: function(e, page) {
+      e.preventDefault()
+      if (0 <= page && page <= this.totalPages)
+        this.currentPage = page - 1
+    },
+    prevPage: function(e) {
+      e.preventDefault()
+      if (this.currentPage > 0)
+        this.currentPage--
+    },
+    nextPage: function(e) {
+      e.preventDefault()
+      if (this.currentPage < this.totalPages - 1)
+        this.currentPage++
+    }
+  }
 }
 </script>
 
